e7f4db4a2120bb8c54a374dbb930a0e21f86ebd6
ujwalp1994 <ujwalpathak1994@gmail.com>
2017-07-12 22:48:50 +0530

    Update SingleTest.java

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -10,7 +10,7 @@ public class SingleTest extends BrowserStackTestNGTest {
 
     @Test
     public void test() throws Exception {
-        driver.get("https://www.google.com/ncr");
+        driver.get("https://www.yahoo.com");
         WebElement element = driver.findElement(By.name("q"));
         element.sendKeys("BrowserStack");
         element.submit();
